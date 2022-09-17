95425907e4b7285f7bc8bccb2a496be9082ad822
MuhammadBayuChandraAdi <112873238+MuhammadBayuChandraAdi@users.noreply.github.com>
2022-09-16 20:38:50 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Muhammad Bayu Chandra Adi \nA11.2022.14666 \nSMA ADABIAH 2 PADANG" << endl;
+    //integer
+    int x = 2304;
+    cout << "Hello gays" << x;
+    //printf("duid");
+    //integer
+    int u = 18;
+    float t = 180;
+    char b = 'lL' ;
+
+    cout <<"umur saya: " <<u;
+    cout <<"\nTinggi saya: " <<t;
+    cout <<"\nUkuran baju: " <<b;
     return 0;
 }
