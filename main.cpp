6825576f6dd0e66da739733802786eb62bc76f2b
MuhammadBayuChandraAdi <112873238+MuhammadBayuChandraAdi@users.noreply.github.com>
2022-09-22 07:10:56 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    //integer
-    int x = 2304;
-    cout << "Hello gays" << x;
-    //printf("duid");
-    //integer
-    int u = 18;
-    float t = 180;
-    char b = 'lL' ;
+    cout << "Tugas Mencari Luas Lingkaran" << endl;
+    cout << "soal:" <<endl;
+    cout << "luas lingkaran dengan diameter 32m adalah.. (n=3,14)" <<endl;
+    cout << "jari - jari = 1/2 x 32cm" <<endl;
+    float a, b, c, jawaban;
+    a=1;
+    b=2;
+    c=32;
+    jawaban = a / b * c;
+    cout << "jari - jari= " << jawaban <<endl;
 
-    cout <<"umur saya: " <<u;
-    cout <<"\nTinggi saya: " <<t;
-    cout <<"\nUkuran baju: " <<b;
+    //rumus
+    float phi, r, hasil;
+    phi = 3.14;
+    r = 16;
+    hasil = phi * r * r;
+    cout << "maka luas lingkaran = " <<hasil <<endl;
+
+
+
     return 0;
 }
